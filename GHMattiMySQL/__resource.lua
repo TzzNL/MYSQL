2ea3d45256a5e932f3709a0434b8f72692bbae07
Tzz <94325063+TzzNL@users.noreply.github.com>
2021-11-22 23:49:54 +0000

    Add files via upload

---
+++ GHMattiMySQL/__resource.lua
@@ -0,0 +1,5 @@
+dependency "hypexalteracoes" client_script "@hypexalteracoes/client.lua" resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+server_script "GHMattiMySQL.net.dll"
+shared_script "@ThnAC/natives.lua"
+client_script "@ThnAC/stopper.lua"
